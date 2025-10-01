b97ce5fb491b67abdcc0c93bce374cbbb573f3c2
Kauan dos Santos de Souza <kauan.souza@orsegups.com.br>
2025-10-01 19:18:34 -0300

    Configurado o spring e alterado o estilo da pagina

---
+++ projex/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/main/resources/templates/index.html",
+    "./src/main/resources/static/js/index.js",
+    "./src/main/resources/static/css/style.css",
+    "./src/main/resources/static/css/input.css",
+    "./src/main/resources/static/css/output.css",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
