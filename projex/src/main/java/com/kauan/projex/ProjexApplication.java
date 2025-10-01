3c598f86e7d21568a45b0adbbfd7332194419368
Kauan dos Santos de Souza <kauan.souza@orsegups.com.br>
2025-10-01 16:41:49 -0300

    Substituindo o projeto antigo

---
+++ projex/src/main/java/com/kauan/projex/ProjexApplication.java
@@ -0,0 +1,13 @@
+package com.kauan.projex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjexApplication.class, args);
+	}
+
+}
---
+++ projex/src/test/java/com/kauan/projex/ProjexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kauan.projex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
