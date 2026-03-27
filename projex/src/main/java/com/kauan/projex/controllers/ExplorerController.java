5c73f9b9eeb6e9d3042e31e69c11283546b76b8c
kauan.souza <kauan.souza@orsegups.com.br>
2026-03-27 19:02:36 -0300

    Começado a alteração da interface e na logica do explore

--- projex/src/main/java/com/kauan/projex/controllers/ExplorerController.java
+++ projex/src/main/java/com/kauan/projex/controllers/ExplorerController.java
@@ -4,8 +4,12 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
+import com.kauan.projex.model.Certificated;
+import com.kauan.projex.model.InfoProject;
+import com.kauan.projex.service.CardCertificateService;
+import com.kauan.projex.service.CardService;
 import com.kauan.projex.service.ExploreService;
+import java.util.List;
 
 import org.springframework.stereotype.Controller;
 
@@ -13,9 +17,13 @@ import org.springframework.stereotype.Controller;
 @RequestMapping("/explore")
 public class ExplorerController {
     public final ExploreService exploreService;
-
-    public  ExplorerController(ExploreService exploreService){
+    public final CardService cardService;
+    public final CardCertificateService certificateService;
+    
+    public  ExplorerController(ExploreService exploreService, CardCertificateService certificateService, CardService cardService ){
         this.exploreService= exploreService;
+        this.certificateService= certificateService;
+        this.cardService= cardService;
     }
 
     @GetMapping
@@ -26,6 +34,11 @@ public class ExplorerController {
         }else{
             model.addAttribute("explorer", exploreService.listarTodos());
         }
+        List<Certificated> certificados = certificateService.buscarPublish(true);
+        List<InfoProject> projetos = cardService.buscarPorPublicado(true);
+
+        model.addAttribute("projetos",projetos);
+        model.addAttribute("certificados",certificados);
         model.addAttribute("search",search);
         model.addAttribute("pageTitle", "Explorar");
         return "pages/explorer";
